f9b47d1fe711e770b6c8ba9c9b75022a632fe25f
gus ryan <rian.putra2003@gmail.com>
2024-10-12 23:48:54 +0800

    add the finished events components

--- app/src/main/java/com/example/eventlistapp/ui/finished/FinishedViewModel.kt
+++ app/src/main/java/com/example/eventlistapp/ui/finished/FinishedViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.eventlistapp.ui.notifications
+package com.example.eventlistapp.ui.finished
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class FinishedViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
