3e609805829ff26bef7e22f67e0438204e3dc8fe
gus ryan <rian.putra2003@gmail.com>
2024-10-12 23:49:27 +0800

    add the upcoming events components

--- app/src/main/java/com/example/eventlistapp/ui/upcoming/UpcomingViewModel.kt
+++ app/src/main/java/com/example/eventlistapp/ui/upcoming/UpcomingViewModel.kt
@@ -1,10 +1,10 @@
-package com.example.eventlistapp.ui.dashboard
+package com.example.eventlistapp.ui.upcoming
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class UpcomingViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"
