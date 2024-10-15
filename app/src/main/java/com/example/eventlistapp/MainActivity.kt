3839d59b1ce600f714899acbc05016fc86315315
gus ryan <rian.putra2003@gmail.com>
2024-10-15 22:19:20 +0800

    set up the data and main class

--- app/src/main/java/com/example/eventlistapp/MainActivity.kt
+++ app/src/main/java/com/example/eventlistapp/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.eventlistapp
 
 import android.os.Bundle
+import android.view.KeyEvent
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
@@ -26,10 +28,24 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+                R.id.navigation_home, R.id.navigation_upcoming, R.id.navigation_finished
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            // Navigate back using the NavController
+            val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+            val navController = navHostFragment.navController
+
+            if (navController.previousBackStackEntry != null) {
+                navController.popBackStack() // Go back to the previous fragment
+                return true // Indicate that the event has been handled
+            }
+        }
+        return super.onKeyDown(keyCode, event) // Handle other key events
     }
 }
