523f17ad22b48bbffe245c472a6b0d877f5b8add
gus ryan <rian.putra2003@gmail.com>
2024-10-26 22:09:39 +0800

    Update the main activity

--- app/src/main/java/com/example/eventlistapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/eventlistapp/ui/MainActivity.kt
@@ -1,22 +1,43 @@
-package com.example.eventlistapp
+package com.example.eventlistapp.ui
 
 import android.os.Bundle
 import android.view.KeyEvent
+import androidx.activity.viewModels
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.eventlistapp.R
+import com.example.eventlistapp.ui.setting.SettingPreferences
 import com.example.eventlistapp.databinding.ActivityMainBinding
+import com.example.eventlistapp.ui.setting.SettingViewModel
+import com.example.eventlistapp.ui.setting.SettingViewModelFactory
+import com.example.eventlistapp.ui.setting.dataStore
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val settingViewModel: SettingViewModel by viewModels {
+        SettingViewModelFactory(SettingPreferences.getInstance(dataStore))
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Observe the theme setting
+        // Observe the theme setting and apply the appropriate mode
+        settingViewModel.getThemeSettings().observe(this) { isDarkModeActive ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
