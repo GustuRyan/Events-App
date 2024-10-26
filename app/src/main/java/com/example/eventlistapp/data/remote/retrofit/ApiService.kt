53f85f0e4a6a85a4bbc6f61d97331f70bf2ee378
gus ryan <rian.putra2003@gmail.com>
2024-10-26 22:07:59 +0800

    Add api service

--- app/src/main/java/com/example/eventlistapp/data/remote/retrofit/ApiService.kt
+++ app/src/main/java/com/example/eventlistapp/data/remote/retrofit/ApiService.kt
@@ -1,25 +1,39 @@
 package com.example.eventlistapp.data.remote.retrofit
 
-import com.example.eventlistapp.data.remote.response.Events
-import com.example.eventlistapp.data.remote.response.Response
-import retrofit2.Call
+import com.example.eventlistapp.data.remote.response.ApiResponse
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ApiService {
-    @GET("events")
-    fun getEvents(@Query("active") active: Int): Call<Response>
 
     @GET("events")
-    fun getEventsLimit(
+    suspend fun getEvents(@Query("active") active: Int): Response<ApiResponse>
+
+    @GET("events")
+    suspend fun getEventsLimit(
         @Query("active") active: Int,
         @Query("limit") limit: Int
-    ): Call<Response>
+    ): Response<ApiResponse>
 
     @GET("events")
-    fun searchEvents(@Query("q") query: String): Call<Response>
+    suspend fun searchEvents(@Query("q") query: String): Response<ApiResponse>
 
     @GET("events/{id}")
-    fun getEventDetail(@Path("id") id: Int): Call<Events>
+    suspend fun getEventDetail(@Path("id") id: Int): Response<ApiResponse>
+
+    companion object {
+        private const val BASE_URL = "https://event-api.dicoding.dev/"
+
+        fun create(): ApiService {
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApiService::class.java)
+        }
+    }
 }
