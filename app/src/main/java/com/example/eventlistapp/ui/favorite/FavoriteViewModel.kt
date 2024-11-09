05d4180dab2f65439c8d746ae2dd03f1ae62c8f6
gus ryan <rian.putra2003@gmail.com>
2024-11-09 14:59:59 +0800

    Add favorite view model and the factory

--- app/src/main/java/com/example/eventlistapp/ui/favorite/FavoriteViewModel.kt
+++ app/src/main/java/com/example/eventlistapp/ui/favorite/FavoriteViewModel.kt
@@ -1,19 +1,15 @@
 package com.example.eventlistapp.ui.favorite
 
 import android.app.Application
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import com.example.eventlistapp.data.local.entity.Favorite
 import com.example.eventlistapp.repository.FavoriteRepository
 
 class FavoriteViewModel(application: Application) : ViewModel() {
     private val mFavoriteRepository: FavoriteRepository = FavoriteRepository(application)
-    fun insert(favorite: Favorite) {
-        mFavoriteRepository.insert(favorite)
-    }
-    fun update(favorite: Favorite) {
-        mFavoriteRepository.update(favorite)
-    }
-    fun delete(favorite: Favorite) {
-        mFavoriteRepository.delete(favorite)
+
+    fun getAllFavoritesLiveData(): LiveData<List<Favorite>> {
+        return mFavoriteRepository.getAllFavoritesLiveData()
     }
 }
